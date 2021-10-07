66fcc45c991a41add7a6ddc2ab1765011f6b4524
Andy Chen <acchen@cpp.edu>
2021-10-06 23:25:51 -0700

    added phone and salary comparision function

--- Employee/employeeTwo.c
+++ Employee/employeeTwo.c
@@ -26,6 +26,16 @@ static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableVa
    return strcmp(tableValuePtr->name, (char *) targetPtr);
 }
 
+static int compareEmployeePhone(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+   return strcmp(tableValuePtr->phone, (char *)targetPtr);
+}
+
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+   return * (long *) targetPtr != tableValuePtr->salary;
+}
+
 PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number)
 {
    return searchEmployeeTable(ptr, size, &number, compareEmployeeNumber);
@@ -34,4 +44,14 @@ PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long numb
 PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char *name)
 {
    return searchEmployeeTable(ptr, size, name, compareEmployeeName);
+}
+
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char *phone)
+{
+   return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
+}
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary)
+{
+   return searchEmployeeTable(ptr, size, &salary, compareEmployeeSalary);
 }
